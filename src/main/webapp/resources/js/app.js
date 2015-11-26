89f8f7baf9f603e5baab64fcaf8ee59073950cda
Daniel Stoian <Daniel Stoian@192.168.204.73>
2015-11-26 17:52:01 +0200

    Update on UI

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -6,4 +6,17 @@ app.controller('Main', function() {
 });
 
 
+
+app.controller('CountryCtrl', function ($scope, $http) {
+
+   $http.get('countries.json').success(function(data) {
+
+       $scope.countries = data;
+
+   });
+
+});
+
+
+
 })();
