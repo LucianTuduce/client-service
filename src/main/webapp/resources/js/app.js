4adb66f56e39d373457eb1ad5321a7cfa54d7313
Daniel Stoian <Daniel Stoian@192.168.204.73>
2015-11-27 14:42:43 +0200

    Added indexJSF.xhtml

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -1,22 +1,32 @@
-(function(){
-var app=angular.module('login', []);
+angular.module('UVSClient', [])
+    .controller('Main', function() {})
 
-app.controller('Main', function() {
+	.controller('CarSearchController', function($scope, $http) {
+	$scope.cars = [];
+	$scope.SearchOptions = ''; //cate una pt fiecare
+	$http.get('cars.json').success(function(data) {
+    	$scope.cars = data;
+	});
 
-});
+	$scope.search = function(event) {
+   		 $http.post('cars.json', {
+       		fin: $scope.FIN,
+        	model: $scope.model,
+        	fuelType: $scope.FuelType,
+        	engineCapacitMin: $scope.CapacityMin,
+        	engineCapacityMax: $scope.CapacityMax,
+        	yearMin: $scope.YearMin,
+        	yearMax: $scope.YearMax,
+        	location: $scope.carLocation,
+        	priceMin: $scope.PriceMin,
+        	priceMax: $scope.PriceMax,
+        	vehicleType: $scope.VehicleType
+    		}).success(function(data) {
+        		$scope.cars = data;
+    		});
+	}	
+
+	});
 
 
 
-app.controller('CountryCtrl', function ($scope, $http) {
-
-   $http.get('countries.json').success(function(data) {
-
-       $scope.countries = data;
-
-   });
-
-});
-
-
-
-})();
