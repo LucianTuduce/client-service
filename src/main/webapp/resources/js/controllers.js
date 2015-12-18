be57de6bd76bc035504801c733aec4511251960e
Daniel Stoian <Daniel Stoian@192.168.204.73>
2015-12-18 14:00:26 +0200

    Update services

--- src/main/webapp/resources/js/controllers.js
+++ src/main/webapp/resources/js/controllers.js
@@ -1,157 +1,100 @@
-angular.module('UVSClientApp')  
-.controller('LoginController',
-    ['$scope', '$rootScope', '$location', 'AuthenticationService',
+angular.module('UVSClientApp')
+    .controller('LoginController', ['$scope', '$rootScope', '$location', 'AuthenticationService',
     function ($scope, $rootScope, $location, AuthenticationService) {
-        // reset login status
-        AuthenticationService.ClearCredentials();
-  
-        $scope.login = function () {
-            //$scope.dataLoading = true;
-            AuthenticationService.Login($scope.username, $scope.password, function(response, status, headers, config) {   
-                if(status == 200) {
-                  console.log("controller success");                  
-                    AuthenticationService.SetCredentials(headers);  //save token for future requests
-                    $location.path('/index'); //go to index page
-                } else {
-                    console.log("controller error");
-                    $scope.error = "Username or passwords invalid";
-                   // $scope.dataLoading = false;
-                } 
-            });
-        };
+            // reset login status
+            AuthenticationService.ClearCredentials();
+
+            $scope.login = function () {
+                //$scope.dataLoading = true;
+                AuthenticationService.Login($scope.username, $scope.password, function (response, status, headers, config) {
+                    if (status == 200) {
+                        console.log("controller success");
+                        AuthenticationService.SetCredentials(headers); //save token for future requests
+                        $location.path('/index'); //go to index page
+                    } else {
+                        console.log("controller error");
+                        $scope.error = "Username or passwords invalid";
+                        // $scope.dataLoading = false;
+                    }
+                });
+            };
     }]);
 
 
 
 angular.module('UVSClientApp')
-.controller('HeaderController', function($scope, $rootScope, Scopes) {
-    Scopes.store('HeaderController', $scope);
+    .controller('HeaderController', function ($scope, $rootScope, Scopes) {
+        Scopes.store('HeaderController', $scope);
 
-    var VehicleTypeVar = "";
-    var CountryVar = "";
-    var LanguageVar = "";
-    $scope.VehicleTypeSelect = function(event) {
-        VehicleTypeVar = event.target.text;
-        $scope.vehicleType = VehicleTypeVar;
-        var parentElement = angular.element(document.querySelector('#VehicleType'));
-        // parentElement.html('<span class="glyphicon glyphicon-home"></span><span class="caret"></span><br/>' + VehicleTypeVar);
-        parentElement.html(VehicleTypeVar);
-    };
+        var VehicleTypeVar = "";
+        var CountryVar = "";
+        var LanguageVar = "";
+        $scope.VehicleTypeSelect = function (event) {
+            VehicleTypeVar = event.target.text;
+            $scope.vehicleType = VehicleTypeVar;
+            var parentElement = angular.element(document.querySelector('#VehicleType'));            
+            parentElement.html(VehicleTypeVar);
+        };
 
-    $scope.CountrySelect = function(event) {
-        CountryVar = event.target.text;
-        $scope.Country = CountryVar;
-        var parentElement = angular.element(document.querySelector('#CountryLanguage'));
-        //parentElement.html('<span class="glyphicon glyphicon-globe"></span><span class="caret"></span><br/>' + CountryVar);
-        parentElement.html(CountryVar);
-    };
+        $scope.CountrySelect = function (event) {
+            CountryVar = event.target.text;
+            $scope.Country = CountryVar;
+            var parentElement = angular.element(document.querySelector('#CountryLanguage'));            
+            parentElement.html(CountryVar);
+        };
 
-    $scope.LanguageSelect = function(event) {
-        LanguageVar = event.target.text;
-        $scope.Language = LanguageVar;
-        var parentElement = angular.element(document.querySelector('#CountryLanguage'));
-        parentElement.html(LanguageVar);
+        $scope.LanguageSelect = function (event) {
+            LanguageVar = event.target.text;
+            $scope.Language = LanguageVar;
+            var parentElement = angular.element(document.querySelector('#CountryLanguage'));
+            parentElement.html(LanguageVar);
 
-    };
-});
+        };
+    });
+
+
+angular.module('UVSClientApp')
+    .controller('CarSearchController', ['$scope', 'Scopes', 'CarSearchService', function ($scope, Scopes, CarSearchService) {
+        Scopes.store('CarSearchController', $scope);
+        $scope.cars = [];
+
+        $scope.search = function () {
+            CarSearchService.CarSearch($scope.FIN, $scope.model, $scope.FuelType, $scope.CapacityMin, $scope.CapacityMax, $scope.YearMin, $scope.YearMax, $scope.PriceMin, $scope.PriceMax, Scopes.get('HeaderController').Country, Scopes.get('HeaderController').vehicleType, function (response, status, headers, config) {
+                if (status == 200) {
+                     console.log("Car Search Result success");
+                    //$scope.carsRetrieved = response.vehicles;
+                } else {
+                    console.log("Car Search Result could not be retrieved");
+                }
+            })
+        };
+           }]);
+        
+ 
 
 
 
 angular.module('UVSClientApp')
-.controller('CarSearchController', function($scope, $rootScope, Scopes, $http) {
-    Scopes.store('CarSearchController', $scope);
-    $scope.cars = [];
-
-    $scope.search = function(event) {
-        var FINVar = $scope.FIN;
-        var modelVar = $scope.model;
-        var FuelTypeVar = $scope.FuelType;
-        var CapacityMinVar = $scope.CapacityMin;
-        var CapacityMaxVar = $scope.CapacityMax;
-        var YearMinVar = $scope.YearMin;
-        var YearMaxVar = $scope.YearMax;
-        var PriceMinVar = $scope.PriceMin;
-        var PriceMaxVar = $scope.PriceMax;
-
-        if ($scope.FIN == undefined) {
-            FINVar = " ";
-        }
-        if ($scope.model == undefined) {
-            modelVar = " ";
-        }
-        if ($scope.FuelType == undefined) {
-            FuelTypeVar = "DEFAULT";
-        }
-        if ($scope.CapacityMin == undefined) {
-            CapacityMinVar = 0;
-        }
-        if ($scope.CapacityMax == undefined) {
-            CapacityMaxVar = 30000;
-        }
-        if ($scope.YearMin == undefined) {
-            YearMinVar = 1900;
-        }
-        if ($scope.YearMax == undefined) {
-            YearMaxVar = 2015;
-        }
-        if ($scope.PriceMin == undefined) {
-            PriceMinVar = 0;
-        }
-        if ($scope.PriceMax == undefined) {
-            PriceMaxVar = 0;
-        }
-
-
-        $http.post('http://localhost:9080/client-service/rest/vehicle/filtered', {
-            fin: FINVar,
-            model: modelVar,
-            fuelType: FuelTypeVar,
-            minCapacity: CapacityMinVar,
-            maxCapacity: CapacityMaxVar,
-            minYear: YearMinVar,
-            maxYear: YearMaxVar,
-            location: Scopes.get('HeaderController').Country,
-            minPrice: PriceMinVar,
-            maxPrice: PriceMaxVar,
-            vehicleType: Scopes.get('HeaderController').vehicleType,
-            pagination: {
-                pageNumber: 1,
-                elemetsPerPage: 20
-            }
-        }).success(function(data) {
-            $scope.carsRetrieved = data.vehicles;
-            $rootScope.$emit("CallParentMethod", {}); //trigger function on CarResultController
-
-
-
-        });
-    }
-});
-
-
-angular.module('UVSClientApp').controller('SearchHistoryController', function($scope, $rootScope, Scopes, $http) {
-    $http({
-        method: 'GET',
-        url: 'http://localhost:9080/client-service/rest/vehicle/search/history'        
+    .controller('SearchHistoryController', ['$scope', '$rootScope','SearchHistoryService', function ($scope, $rootScope, SearchHistoryService) {
+        //get search history on page load
+        SearchHistoryService.GetSearchHistory(function(response) {
+            $scope.searches = response.data;            
+        });        
         
-    }).then(function(response) {
-        $scope.searches = response.data;
-    });
-
-    $rootScope.$on("CallParentMethod", function() { //Listen for trigger
-        $http({
-            method: 'GET',
-            url: 'http://localhost:9080/client-service/rest/vehicle/search/history'
-        }).then(function(response) {
-            $scope.searches = response.data;
+        //reload the search history once a new search is made
+        $rootScope.$on("CarSearchMethod", function () { //Listen for trigger
+            SearchHistoryService.GetSearchHistory(function(response) {
+            $scope.searches = response.data;            
         });
-    })
-});
+        })
+    }]);
 
 
-angular.module('UVSClientApp').controller('CarResultController', function($scope, $rootScope, Scopes, $http) {
-    $rootScope.$on("CallParentMethod", function() { //Listen for trigger
-        $scope.cars = Scopes.get('CarSearchController').carsRetrieved;
-        console.log($scope.cars);
+angular.module('UVSClientApp')
+    .controller('CarResultController', function ($scope, $rootScope) {
+        //load the search results once a new search is made
+        $rootScope.$on("CarSearchMethod", function () { //Listen for trigger
+            $scope.cars = $rootScope.carsRetrieved;
+           
+        });
     });
-});
--- src/main/webapp/resources/js/services.js
+++ src/main/webapp/resources/js/services.js
@@ -1,48 +1,128 @@
-angular.module('UVSClientApp')  
-.factory('AuthenticationService',
-    ['$http', '$rootScope', '$timeout',
-    function ( $http, $rootScope, $timeout) {
+angular.module('UVSClientApp').factory('AuthenticationService', ['$http', '$rootScope', '$timeout',
+    function ($http, $rootScope, $timeout) {
         var service = {};
- 
-        service.Login = function (username, password, callback) {           
- 
+
+        service.Login = function (username, password, callback) {
+
             /* Use this for real authentication
              ----------------------------------------------*/
-            $http.post('http://localhost:9080/client-service/rest/user/login', { username: username, password: password })
+            $http.post('http://localhost:9080/client-service/rest/user/login', {
+                    username: username,
+                    password: password
+                })
                 .success(function (response, status, headers, config) {
-                  callback(response, status, headers().authorization, config); // header() function because header is returned as a function
-                  
-               }).error(function (response, status, headers, config) {
-                  callback(response, status, headers().authorization, config);
-               });
- 
+                    callback(response, status, headers().authorization, config); // header() function because header is returned as a function
+
+                }).error(function (response, status, headers, config) {
+                    callback(response, status, headers().authorization, config);
+                });
+
         };
-  
+
         service.SetCredentials = function (authorization) {
-            var authdata = authorization;             
+            var authdata = authorization;
             $http.defaults.headers.common['Authorization'] = authdata; // here the token is saved for future use           
         };
-        
-  
-        service.ClearCredentials = function () { 
-            $http.defaults.headers.common.Authorization = '';  //delete the token          
+
+
+        service.ClearCredentials = function () {
+            $http.defaults.headers.common.Authorization = ''; //delete the token          
         };
-  
+
         return service;
     }]);
 
 
-angular.module('UVSClientApp')
-/*  */
+angular.module('UVSClientApp').factory('CarSearchService', ['$http', '$rootScope', '$timeout',
+    function ($http, $rootScope, $timeout) {
+        var service = {};
+
+        service.CarSearch = function (FINVar, modelVar, FuelTypeVar, CapacityMinVar, CapacityMaxVar, YearMinVar, YearMaxVar, PriceMinVar, PriceMaxVar, CountryVar, vehicleTypeVar, callback) {
+            if (FINVar == undefined || FINVar == '') {
+                FINVar = " ";
+            }
+            if (modelVar == undefined) {
+                modelVar = " ";
+            }
+            if (FuelTypeVar == undefined) {
+                FuelTypeVar = "DEFAULT";
+            }
+            if (CapacityMinVar == undefined || CapacityMinVar == '') {
+                CapacityMinVar = 0;
+            }
+            if (CapacityMaxVar == undefined || CapacityMaxVar == '') {
+                CapacityMaxVar = 30000;
+            }
+            if (YearMinVar == undefined || YearMinVar == '') {
+                YearMinVar = 1900;
+            }
+            if (YearMaxVar == undefined || YearMaxVar == '') {
+                YearMaxVar = 2015;
+            }
+            if (PriceMinVar == undefined || PriceMinVar == '') {
+                PriceMinVar = 0;
+            }
+            if (PriceMaxVar == undefined || PriceMaxVar == '') {
+                PriceMaxVar = 0;
+            }
+
+            $http.post('http://localhost:9080/client-service/rest/vehicle/filtered', {
+                fin: FINVar,
+                model: modelVar,
+                fuelType: FuelTypeVar,
+                minCapacity: CapacityMinVar,
+                maxCapacity: CapacityMaxVar,
+                minYear: YearMinVar,
+                maxYear: YearMaxVar,
+                location: CountryVar,
+                //location: Scopes.get('HeaderController').Country,
+                minPrice: PriceMinVar,
+                maxPrice: PriceMaxVar,
+                vehicleType: vehicleTypeVar,
+                //vehicleType: Scopes.get('HeaderController').vehicleType,
+                pagination: {
+                    pageNumber: 1,
+                    elemetsPerPage: 20
+                }
+            }).success(function (response, status, headers, config) {
+                callback(response, status, headers().authorization, config);
+                $rootScope.carsRetrieved = response.vehicles; //store the results
+                $rootScope.$emit("CarSearchMethod", {}); //trigger function on CarResultController
+
+            }).error(function (response, status, headers, config) {
+                callback(response, status, headers().authorization, config);
+            });
+        };
+        return service;
+                }]);
+
+
+
+angular.module('UVSClientApp').factory('SearchHistoryService', ['$http', '$rootScope', '$timeout',
+    function ($http, $rootScope, $timeout) {
+        var service = {};
+
+        service.GetSearchHistory = function (callback) {
+            $http({
+                method: 'GET',
+                url: 'http://localhost:9080/client-service/rest/vehicle/search/history'
+            }).then(function (response) {
+                callback(response);
+                //$scope.searches = response.data;
+            });
+        };
+        return service;
+    }]);
+
 
 /* factory to connect controllers */
-.factory('Scopes', function($rootScope) {
+angular.module('UVSClientApp').factory('Scopes', function ($rootScope) {
     var mem = {};
     return {
-        store: function(key, value) {
+        store: function (key, value) {
             mem[key] = value;
         },
-        get: function(key) {
+        get: function (key) {
             return mem[key];
         }
     };
