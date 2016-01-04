dcd930a97410c4b9543e7566877be7dca5b57de8
Daria <dariaboom@yahoo.com>
2016-01-04 15:24:06 +0200

    Validation for addCar in Rest

--- src/main/java/ro/fortech/model/Vehicle.java
+++ src/main/java/ro/fortech/model/Vehicle.java
@@ -1,8 +1,5 @@
 package ro.fortech.model;
 
-import javax.validation.constraints.NotNull;
-
-import ro.fortech.constraints.ConstraintUnicity;
 import ro.fortech.type.FuelType;
 import ro.fortech.type.VehicleType;
 
@@ -12,10 +9,7 @@ import ro.fortech.type.VehicleType;
  */
 public class Vehicle {
 
-	@NotNull
-	@ConstraintUnicity
 	private String fin;
-	
 	private String model;
 	private FuelType fuelType;
 	private int engineCapacity;
@@ -24,12 +18,9 @@ public class Vehicle {
 	private int price;
 	private VehicleType vehicleType;
 	
-	
 	public String getFin() {
 		return fin;
 	}
-	@NotNull
-	@ConstraintUnicity
 	public void setFin(String fin) {
 		this.fin = fin;
 	}
