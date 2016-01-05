2563b1142d51508ca0cba0f03b0218dce2b8b9db
Lucian Tuduce <lucian.tuduce@fortech.ro>
2016-01-05 15:05:29 +0200

    Code refactor

--- src/main/java/ro/fortech/faces/NotNullValidator.java
+++ src/main/java/ro/fortech/faces/NotNullValidator.java
@@ -17,8 +17,7 @@ import org.apache.commons.lang3.StringUtils;
 public class NotNullValidator implements Validator {
 
 	@Override
-	public void validate(FacesContext context, UIComponent component,
-			Object value) throws ValidatorException {
+	public void validate(FacesContext context, UIComponent component,Object value) throws ValidatorException {
 
 		String valueToBeChecked = String.valueOf(value);
 			
@@ -29,9 +28,7 @@ public class NotNullValidator implements Validator {
 		}
 				
 	if (!valid) {
-			FacesMessage message = new FacesMessage(
-					FacesMessage.SEVERITY_ERROR, "The field must be filled",
-					"The field must be filled");
+			FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "The field must be filled", "The field must be filled");
 			throw new ValidatorException(message);
 		}
 	}
